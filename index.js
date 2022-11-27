8d7f84a75c56cd248aa65259e6d0fd4eb9d503b2
PhamPhuTuan <phamphutuan0710pt@gmail.com>
2022-11-28 00:44:32 +0700

    update

--- index.js
+++ index.js
@@ -1,25 +1,60 @@
 const express = require('express')
 const app = express()
-
 const http = require('http')
 const server = http.createServer(app)
 const {Server} = require('socket.io')
 
+const MessageSchema = require("./MessageSchema")
+const mongoose = require('mongoose')
+
 const io = new Server(server)
 
-app.get('/chat', (req, res) => {
+// Connect DB
+mongoose.connect('mongodb+srv://phutuan:1234@cluster0.u0olj5k.mongodb.net/test',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+.then(()=>console.log("connect DB successfull"))
+.catch((err)=>console.log(err));
+///
+app.use(express.json());
+// Post
+app.post('/', async (req, res) => {
+    const newMessage = new MessageSchema(req.body);
+    try {
+        const savedMessage = await newMessage.save();
+        res.status(200).json(savedMessage);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    //   io.emit('on-chat', {
+    //     message: req.body
+    // })
+    // io.on('connection', (socket) => {
+    //     socket.on('on-chat', data => {
+    //         io.emit('user-chat', data)
+    //     })
+    // })
+})
+
+app.get('/', async (req, res) =>{
     res.sendFile(__dirname+'/index.html')
+    // try {
+    //     const Message=await MessageSchema.find();
+    //     res.status(200).json(Message)
+    // }
+    // catch(e){
+    //     res.status(500).json(e)
+    // }
 })
 
 io.on('connection', (socket) => {
     console.log('connected')
     socket.on('on-chat', data => {
-        console.log("event")
-        console.log(data)
         io.emit('user-chat', data)
     })
 })
 
-server.listen(process.env.PORT, () => {
+server.listen(2999, () => {
     console.log('listening on port 2999')
 })
